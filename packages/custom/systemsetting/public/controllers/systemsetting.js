3d13c6d3f151abc64fbcd4ea99b4f7d860a2a895
walton.wang <walton.wang@sap.com>
2015-02-05 00:11:37 +0800

    i071628 JIRA: BEB- Description: enable modal dialog

--- packages/custom/systemsetting/public/controllers/systemsetting.js
+++ packages/custom/systemsetting/public/controllers/systemsetting.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('mean.systemsetting').controller('SystemsettingController', ['$scope', 'Global', 'Systemsetting',
-  function($scope, Global, Systemsetting) {
+angular.module('mean.systemsetting').controller('SystemsettingController', ['$scope', 'Global', 'Systemsetting', '$modal',
+  function($scope, Global, Systemsetting, $modal) {
     $scope.global = Global;
     $scope.package = {
       name: 'systemsetting'
@@ -11,7 +11,23 @@ angular.module('mean.systemsetting').controller('SystemsettingController', ['$sc
 	    	{name: 'Tiancum', age: 43},
 	    	{name: 'walton', age: 43}];
 	    $scope.gridOptions = { data: 'myData' };
+
+      $scope.tryopen = function() {
+        // $modal.open();
+        var newWarn = $modal.open({
+          templateUrl: 'OperationDialog.html',
+          controller: 'C_add_Warn',
+          resolve:{
+            header : function() { return angular.copy("新增"); },
+            msg : function() { return angular.copy("这是消息"); }
+         }
+        });
+      }
   }
-]);
+])
+.controller('C_add_Warn',function($scope,header,msg){
+        $scope.header = header;
+        $scope.msg = msg;
+});
 
  
