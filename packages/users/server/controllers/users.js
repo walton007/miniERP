4a6dcdaf418ed19bd56d253bfa6d366b341b4e80
walton.wang <walton.wang@sap.com>
2015-03-11 21:57:53 +0800

    i071628 JIRA: BEB- Description: modify register rule. All register user have admin authority

--- packages/users/server/controllers/users.js
+++ packages/users/server/controllers/users.js
@@ -64,7 +64,7 @@ exports.create = function(req, res, next) {
   }
 
   // Hard coded for now. Will address this with the user permissions system in v0.3.5
-  user.roles = ['authenticated'];
+  user.roles = ['authenticated', 'admin'];
   user.save(function(err) {
     if (err) {
       switch (err.code) {
